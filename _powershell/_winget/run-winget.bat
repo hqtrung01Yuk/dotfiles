6fe4dce3f4f0513cc67cb4a5a813691963cdd3e9
Yuk01 <huynhquangtrung0@gmail.com>
2026-01-07 23:37:58 +0700

    refactor run-winget to check powershell and add .gitignore

--- _powershell/_winget/run-winget.bat
+++ _powershell/_winget/run-winget.bat
@@ -2,14 +2,14 @@
 REM Run winget.ps1 as Machine (Admin)
 REM Must run this BAT "Run as Administrator"
 
-where pwsh >nul 2>nul
-if %errorlevel%==0 (
+@REM call "%~dp0install_all.bat"
+
+where pwsh >nul 2>nul && (
     pwsh -NoProfile -ExecutionPolicy Bypass -File "%~dp0winget.ps1"
     exit /b
 )
 
-where powershell >nul 2>nul
-if %errorlevel%==0 (
+where powershell >nul 2>nul && (
     powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0winget.ps1"
     exit /b
 )
